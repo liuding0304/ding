2199a3debbf234f5247ac22e71b8e344483d26fb
deandliu <deandliu@tencent.com>
2022-12-14 15:25:46 +0800

    update

--- scripts/build.js
+++ scripts/build.js
@@ -1,7 +1,23 @@
 const execSync = require('child_process').execSync;
-execSync('pnpm run  --filter article  docs:build')
-// 构建article
+const fse = require('fs-extra');
+const path = require('path');
 
-// 构建blog
+const stdio = { stdio: 'inherit' };
+
+function resolve(p) {
+  return path.resolve(__dirname, p)
+}
+
+
+
+// // 构建article
+// execSync('pnpm run -F article docs:build', stdio)
+
+// // 构建blog
+// execSync('pnpm run -F blog build', stdio)
 
 // 组合构建结果输入到dist
+fse.ensureDirSync(resolve('../dist'))
+fse.copySync(resolve('../packages/article/dist'), resolve('../dist'))
+fse.copySync(resolve('../packages/article/dist'), resolve('../dist'))
+fse.copySync(resolve('../packages/blog/dist'), resolve('../dist'))
